8ff3cdcbc64df61335eb8fe65e1635a89950a607
Md Zahirul Islam Mehadi <108610227+Programmer-Mehadi@users.noreply.github.com>
2023-12-19 14:21:43 +0600

    Setup

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+/* eslint-disable @next/next/no-title-in-document-head */
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>NFTERS</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
