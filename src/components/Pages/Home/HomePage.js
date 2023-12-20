21c895cb220371589eb74130d8e4dfe80397f3be
Md Zahirul Islam Mehadi <108610227+Programmer-Mehadi@users.noreply.github.com>
2023-12-20 15:06:41 +0600

    [DONE] The Amazing Art done.

--- src/components/Pages/Home/HomePage.js
+++ src/components/Pages/Home/HomePage.js
@@ -4,11 +4,13 @@ import React from "react"
 import CollectionFeatured from "./Sections/CollectionFeatured/CollectionFeatured"
 import CreateAndSell from "./Sections/CreateAndSell/CreateAndSell"
 import DiscoverMoreNFTs from "./Sections/DiscoverMoreNFTs/DiscoverMoreNFTs"
+import TheAmazingArt from "./Sections/TheAmazingArt/TheAmazingArt"
 
 export default function HomePage() {
   return (
     <section>
       <Header />
+      <TheAmazingArt />
       <CollectionFeatured />
       <CreateAndSell />
       <DiscoverMoreNFTs />
